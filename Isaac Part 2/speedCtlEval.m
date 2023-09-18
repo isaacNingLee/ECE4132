f30cf73c874276ea2c71bd70c38ceaf8fe96b0d4
isaacNingLee <ilee0022@student.monash.edu>
2023-09-18 10:50:23 +1000

    new changes

---
+++ Isaac Part 2/speedCtlEval.m
@@ -0,0 +1,144 @@
+% This script runs the single-vehicle control design part of the ECE4132 project
+% It does the following basic steps:
+% SETUP:
+% -- chooses a control task to evaluate (task 1, 2, or 3). Change as needed
+% -- generates road slope function
+% -- generates a vehicle model (including initial nominal velocity)
+% -- generates a speed reference signal 
+% SYSID data: 
+% -- generates step response data from the model
+% -- calls YOUR function (sysID4Ctl) that finds an LTI vehicle model 
+% CONTROL design:
+% -- calls YOUR function (cltDesignSingle) that returns LTI controller sys
+% EVALUATION:
+% -- runs the closed-loop system in three scenarios:
+%    -- constant reference velocity and changing slope (if speedList has a
+%    single element)
+%    -- constant zero slope and changing reference velocity (if maxSlope=0)
+%    -- both changing reference velocity and changing road slope
+%    (otherwise)
+% -- RESULTS for each of the three cases:
+%    -- plots the vehicle velocity response, the reference 
+%    and performance metric, as feedback
+% author: James Saunderson, August 2023.
+
+clearvars;
+output = zeros(100,5);
+for i = [1:10]
+    i
+    close all;
+    load_system('vehicleCtl.slx');
+    load_system('vehicleID.slx');
+
+    %%%%%%%% 
+    % The control task. Set this to either 1, 2, or 3 depending on which task
+    % you are testing.
+    task = 3;
+
+    % set nominal velocity for the control problem
+    if task==2
+        nomVel = 60;
+    else 
+        nomVel = 50+rand()*50; % nominal velocity between 50 and 100
+    end
+
+    %%%%%%%% system ID for control %%%%%%%%%
+
+    % simulation time for sysID
+    maxTimeID = 900;
+    set_param('vehicleID','StopTime',num2str(maxTimeID));
+
+    % generate a function handle theta that is the road slope (in degrees) 
+    % as a function of position. Theta should be a function from reals to reals
+
+    maxSlope = 0; % for identification use zero slope
+    theta = generateRoadSlope(maxSlope,maxTimeID,'step');
+
+    % generate function handles needed to simulate the vehicle
+    [Ffun,Gfun,vm] = generateVehicleModel4Ctl(theta,nomVel);
+
+    % set model to 0 when initially extracting response data from actual vehicle
+    Gvehicle = tf(0);
+
+    % generate system response data based on numResponses step inputs of
+    % different amounts. 
+    % -- you can change the number of "training" timeseries here if you want to.
+    %    if you want to identify a model from data, then you must change this 
+    %    to be non-zero
+    numResponsesTraining = 2; 
+    % Sample random torque input step increments
+    % -- you can change this if you would like to try different 
+    %    step inputs (rather than random ones) for training data. 
+    % -- this will be a 1 x 0 empty array if numResponsesTraining==0
+    deltaStepsTraining = 0.1*(2*rand(1,numResponsesTraining)-1);
+    [systemInputData,systemResponseData,~] = generateResponseData(Gvehicle,numResponsesTraining,deltaStepsTraining);
+
+    % run YOUR function sysID4Ctl that finds (based on measured system data)
+    % an LTI model that relates torque request deviation to velocity deviation.
+    % Notes: 
+    % -- This can be different from the function you used for system ID, as 
+    %    you may want to use a simpler model to inform your control design.
+    % -- This is optional. You may not want to use any model identified from 
+    %    data to inform your control design. In that case sysID4Ctl can just
+    %    return the zero transfer function, for example.
+    Gvehicle = sysID4Ctl(systemInputData,systemResponseData);
+
+    %%%%%% control design and testing %%%%%%
+
+    % run YOUR function ctlSingleDesign that returns the parameters of an 
+    % controller. The controller it generates takes as input the error 
+    % between the measured and reference velocity and produces as output a 
+    % torque request for the vehicle model.
+    [LTIblock,Ki,Kt,Tmax,Tmin] = ctlDesign(Gvehicle,task);
+
+    % set control simulation time
+    maxTimeCtl = 300;
+    set_param('vehicleCtl','StopTime',num2str(maxTimeCtl));
+
+    % generate velocity reference signal. In task two this is just a constant.
+    if task==2
+        velocityRef = generateVelocityReference(maxTimeCtl,nomVel,'const');
+    else % in the other tasks this has step changes
+        velocityRef = generateVelocityReference(maxTimeCtl,nomVel,'var');
+    end
+
+    % generate a function handle theta that is the road slope (in degrees) 
+    % as a function of position. Theta should be a function from reals to reals
+
+    % for control task 1, set this to 0 degrees (no road slope). 
+    % for control tasks 2 and 3, set this to 6 degrees maximum road slope.
+    if task==1
+        maxSlope = 0;
+    else 
+        maxSlope = 6; % (in degrees) 
+    end
+    theta = generateRoadSlope(maxSlope,maxTimeCtl,'pl');
+    [Ffun,Gfun] = generateVehicleModel4Ctl(theta,nomVel,vm);
+
+    % run closed loop system
+    [torqueRequestData, velocityResponseData,velocityRefData] = runSpeedControl(LTIblock,Ki,Kt,Tmax,Tmin,velocityRef);
+
+    % compute evaluation metric. 
+    % Use values normalized by the reference, so that 
+    % comparisons across different runs make more sense
+    [mseArray,nmseArray] = computeMSE({velocityResponseData},{velocityRefData});
+    [speedingFine,graceProfile] = computeSpeeding(velocityRefData,velocityResponseData);
+    % compute acceleration 
+    accelerationResponse = timeseries(diff(velocityResponseData.Data/3.6)./diff(velocityResponseData.Time),velocityResponseData.Time(1:(end-1)));
+    % compute jerk response 
+    jerkResponse = timeseries(diff(accelerationResponse.Data)./diff(accelerationResponse.Time),accelerationResponse.Time(1:(end-1)));
+
+    % plot results
+    plotResponsesSpeedRefGs(velocityRef,velocityResponseData,graceProfile,sqrt(nmseArray),speedingFine,accelerationResponse,jerkResponse);
+    
+    output(i, 1) = mseArray;
+    output(i, 2) = nmseArray;
+    output(i, 3) = speedingFine;
+    output(i, 4) = max(abs(accelerationResponse.Data));
+    output(i, 5) = max(abs(jerkResponse.Data));
+
+    close_system('vehicleCtl.slx',0);
+    close_system('vehicleID.slx',0);
+end
+
+save('task1.mat',"output")
---
+++ Isaac Part 2/sysID4Ctl.m
@@ -0,0 +1,114 @@
+function vehicleModel = sysIDCtl(inputData,outputData)
+
+% This function takes in two cell arrays of timeseries data and returns an
+% LTI system object (this can be a transfer function or a state space
+% model). 
+%
+% Arguments:
+% -- inputData is a k x 1 cell array where each element is a timeSeries object
+%    - inputData{j} is the timeSeries object corresponding to the jth 
+%      "training" input
+%    - inputData{j}.Time is an array of the simulation time-stamps for the
+%      jth "training" input
+%    - inputData{j}.Data is an array of the corresponding input signal values
+%      for the jth "training" input
+% -- ouputData is a k x 1 cell array where each element is a timeSeries object
+%    - outputData{j} is the timeSeries object corresponding to the jth
+%      "training" output
+%    - outputData{j}.Time is an array of the simulation time-stamps for the
+%      jth "training" output
+%    - outputData{j}.Data is an array of the corresponding input signal values
+%      for the jth "training" output
+%
+% Assumptions:
+% -- the function assumes that all of the inputData{j}.Time vectors and 
+% all of the outputData{j}.Time vectors are identical.
+%
+% Constraints:
+% -- if you want to simulate your model within this function, you are 
+%    expected to use lsim.
+% -- you should ** not ** call the function generateResponseData in this function. 
+%    The only information about the system you should use in this function are the input/output 
+%    timeSeries arrays that are passed as arguments to the function
+% 
+% Hints:
+% -- The system starts in equilibrium. In the data preprocessing given below, 
+%    there is a shift that converts the given signals into the *deviation* 
+%    of the input from equilibrium and the *deviation* of the ouput from 
+%    equilbrium. Your LTI system model works with these deviation signals.
+% -- To access the number of timeSeries objects in inputData you can use
+%    length(inputData). (Similarly for outputData.)
+% -- With a little trick (illustrated in the sample code below), you can run lsim 
+%    on multiple different inputs simultanously (stored as an array) and 
+%    it will return multiple outputs (stored as an array)
+
+% Everything below is just sample code that may or may not be useful.
+% It can be safely removed and replaced with your code.
+
+% this is just a fixed reference vehicle model to get you started. It does not
+% use the input/output data at all!
+
+% DATA PREPROCESSING (may be useful, can be removed if not needed):
+% The following converts the cell arrays of time-series that are passed
+% into the function into arrays that are (# timesteps) x (# timeSeries)
+% and are shifted to remove the equilibrium values, so they are easier 
+% to pass into lsim.
+% -- inputTimesArray is a vector of time-stamps
+% -- inputDataArray is an array of corresponding input signal values 
+%    shifted to represent deviation from equilibrium, with
+%    each column corresponding to a different input signal
+% -- outputDataArray is an array of corrsponding output signal values, 
+%    shifted to represent deviation from equilibrium, with 
+%    each column corresponding to a different output signal
+
+% Obtain the Time Vector from the input data
+inputTimesVector = inputData{1}.Time; 
+
+% Pre-allocate the input data and the output data array and the parameters
+inputDataArray = zeros(length(inputData{1}.Data),length(inputData));
+outputDataArray = zeros(length(outputData{1}.Data),length(outputData));
+
+Tmax = 900;
+% Shift input data to represent deviations from equilibrium
+for i = 1:length(inputData)
+    inputDataArray(:, i) = inputData{i}.Data - inputData{i}.Data(1);
+end
+
+% Shift output data to represent deviations from equilibrium
+for i = 1:length(outputData)
+    outputDataArray(:, i) = outputData{i}.Data - outputData{i}.Data(1);
+end
+
+thetaOpt = zeros(length(inputData),4);
+s = tf("s");
+options = optimset('MaxIter', 1e5, 'MaxFunEvals',1e7);
+% Identify the system parameters (thetaOpt) for each input-output pair using fminsearch
+for i = 1:1:length(inputData)
+    input_data = inputDataArray(:,i);
+    output_data = outputDataArray(:,i);
+    ts = inputTimesVector;
+    v_ss = mean(output_data(end-50:end));
+    
+    % Define a cost function to minimize using fminsearch
+    cost = @(theta) sum((output_data-lsim(theta(1) * (s + theta(2))/(s^2 + theta(3)*s + theta(4)) , input_data, ts)).^2);
+    
+    % Initialise parameter estimates
+    v_init = output_data(1);
+    T63 = (find((abs(output_data-v_init)) >= (abs(v_ss-v_init))*0.63, 1 )-1) * Tmax / length(ts) ;
+    theta_init = [1,1,1,1];
+    % Optimize the cost function to find the best parameter estimates
+    thetaOpt(i,:) = fminsearch(cost, theta_init, options);
+end
+
+% Take the mean of parameter estimates across all input-output pairs
+thetaOpt = mean(thetaOpt);
+
+% Create the identified LTI system model (vehicleModel)
+vehicleModel = thetaOpt(1) * (s + thetaOpt(2))/(s^2 + thetaOpt(3)*s + thetaOpt(4));
+
+% The following is an example of how to use lsim to simulate the response
+% of vehicleModel to all of the inputs, and capture the responses in an array.
+% The same approach may be useful for your systemID method.
+
+% Please comment this out if you do not need to use this code!!
+% modelOutputArray = lsim(vehicleModel * eye(length(inputData)), inputDataArray, inputTimesVector);
